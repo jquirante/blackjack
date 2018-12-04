cff02b07644554624f1305e2bc866beb89f81ad1
Justen Quirante <jcquirante@gmail.com>
2018-12-03 20:45:43 -0800

    Started functionality to get card totals

--- components/player.js
+++ components/player.js
@@ -6,5 +6,27 @@ class Player {
         this.playerCards = [];
     }
 
-    
+    getCardTotal (cardsArray) {
+        var total = null;
+
+        for ( var cardValue = 0; cardValue < cardsArray.length; cardValue ++ ) {
+            switch (val) {
+                case 'A':
+                    total += this.playerCards[cardValue];
+                case '2':
+                case '3':
+                case '4':
+                case '5':
+                case '6':
+                case '7':
+                case '8':
+                case '9':
+                case '10':
+                case 'J':
+                case 'Q':
+                case 'K':
+            }
+        }
+            
+    }
 } 
